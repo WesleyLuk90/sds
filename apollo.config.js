6e488142f0e4395dda73024a7ec4bf405d71fa35
Wesley Luk <wesleyluk@gmail.com>
2019-03-31 14:08:34 -0700

    Remove add type name

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     client: {
         includes: ["./client/src/**/*.ts"],
-        addTypename: false,
         service: {
             localSchemaFile: "./client/graphql-schema.json"
         }
