d595cdcdef529d60237d8443d142578c35750bea
Wesley Luk <wesleyluk@gmail.com>
2019-04-06 12:51:29 -0700

    Fix table

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,4 +1,4 @@
-import MuiTable from "@material-ui/core/Typography";
+import MuiTable from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
