b8c2f4bcf34d1f119c62e03544e6307391b1bd48
Wesley Luk <wesleyluk@gmail.com>
2019-04-06 15:28:43 -0700

    Validate field types

--- server/src/storage/CollectionService.ts
+++ server/src/storage/CollectionService.ts
@@ -6,19 +6,27 @@ import {
     CollectionType
 } from "./Collection";
 
-export class CollectionService {
-    convertFieldType(fieldType: FieldType): CollectionFieldType {
-        switch (fieldType) {
-            case FieldType.ID:
-                return CollectionFieldType.KEYWORD;
-            case FieldType.TEXT:
-                return CollectionFieldType.TEXT;
-        }
-        throw new Error(`Invalid field type ${fieldType}`);
+function convertFieldType(fieldType: FieldType): CollectionFieldType {
+    switch (fieldType) {
+        case FieldType.ID:
+            return CollectionFieldType.KEYWORD;
+        case FieldType.TEXT:
+            return CollectionFieldType.TEXT;
     }
+    throw new Error(`Invalid field type ${fieldType}`);
+}
 
-    toCollectionField(field: Field): CollectionField {
-        return new CollectionField(this.convertFieldType(field.type), field.id);
+function validateFieldTypes() {
+    Object.keys(FieldType)
+        .map(key => FieldType[key as any])
+        .forEach(convertFieldType);
+}
+
+validateFieldTypes();
+
+export class CollectionService {
+    private toCollectionField(field: Field): CollectionField {
+        return new CollectionField(convertFieldType(field.type), field.id);
     }
 
     toCollection(documentType: DocumentType): Collection {
