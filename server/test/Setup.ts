4fe2f8aba53b9d383cab9ed44cb8e10b029f38c5
Wesley Luk <wesleyluk@gmail.com>
2019-04-07 12:14:25 -0700

    Add integration tests

--- server/test/Setup.ts
+++ server/test/Setup.ts
@@ -1,5 +1,5 @@
 describe.integration = function(name, test) {
-    if (process.env["TEST_PROFILE"] === "INTEGRATION") {
+    if (process.env["TEST_PROFILE"] === "INTEGRATION" || process.env["CI"]) {
         describe(name, test);
     } else {
         describe(name, () => {
